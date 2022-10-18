73fe6f1329e0b29c39c89ff93c672735ee44e0d0
Fang Linxun <fanglinxun@gmail.com>
2022-10-18 20:21:09 +0200

    with efinance realtime_quotes

--- pythonprojects/fzgroupinv.py
+++ pythonprojects/fzgroupinv.py
@@ -1,10 +1,38 @@
+import efinance as ef
 
+#ef.stock.get_realtime_quotes()
 
 preHigh = 174.55
 hisHigh = 182.01
 cPrice = float(input('Please input current price: '))
-amount = 180
 
+amount = 300
+    
+def horse_tick(hisHigh, cPrice):
+    if cPrice >= hisHigh * (1-0.05):
+        print('Sell 90% of the shares. To keep {0} shares. Set auto trade with price {1:.2f}'.format(amount * 0.1,\
+            hisHigh * (1-0.05)))
+    if hisHigh * (1-0.05) >= cPrice > hisHigh * (1-0.1):
+        print('Keep 10% of the shares. To keep {0} shares'.format(amount * 0.1))
+    if hisHigh * (1-0.1) >= cPrice > hisHigh * (1-0.15):
+        print('Keep 20% of the shares. To keep {0} shares'.format(amount * 0.2))
+    if hisHigh * (1-0.15) >= cPrice > hisHigh * (1-0.2):
+        print('Keep 30% of the shares. To keep {0} shares'.format(amount * 0.3))    
+    if hisHigh * (1-0.2) >= cPrice > hisHigh * (1-0.25):
+        print('Keep 40% of the shares. To keep {0} shares'.format(amount * 0.4))  
+    if hisHigh * (1-0.25) >= cPrice > hisHigh * (1-0.3):
+        print('Keep 50% of the shares. To keep {0} shares'.format(amount * 0.5))  
+    if hisHigh * (1-0.3) >= cPrice > hisHigh * (1-0.35):
+        print('Keep 60% of the shares. To keep {0} shares'.format(amount * 0.6)) 
+    if hisHigh * (1-0.35) >= cPrice > hisHigh * (1-0.4):
+        print('Keep 70% of the shares. To keep {0} shares'.format(amount * 0.7)) 
+    if hisHigh * (1-0.4) >= cPrice > hisHigh * (1-0.45):
+        print('Keep 80% of the shares. To keep {0} shares'.format(amount * 0.8)) 
+    if hisHigh * (1-0.45) >= cPrice > hisHigh * (1-0.5):
+        print('Keep 90% of the shares. To keep {0} shares'.format(amount * 0.9)) 
+    if hisHigh * (1-0.50) >= cPrice > hisHigh * (1-0.5):
+        print('Keep 100% of the shares. To keep {0} shares'.format(amount * 1))       
+    
 
 def reblancing(preHigh, cPrice):
     if cPrice >= preHigh:
@@ -31,5 +59,17 @@ def reblancing(preHigh, cPrice):
     if preHigh * (1-0.25) >= cPrice:
         print('It is bare market, sell all!!!')
 
+while True:
 
-reblancing(preHigh, cPrice)
+    crash = input('Is the NASDAQ dropped more than 3%?:(y or n)')
+
+    if crash == 'y':
+        horse_tick(hisHigh, cPrice)
+        break
+    if crash == 'n':
+        reblancing(preHigh, cPrice)
+        break
+    else:
+        print('Please input y or n.')
+
+
