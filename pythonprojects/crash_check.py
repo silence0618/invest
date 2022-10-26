b7471bd477cf504a58fc087bf2977721c1e015dd
Fang Linxun <fanglinxun@gmail.com>
2022-10-26 06:57:02 +0200

    auto update crashcheck

--- pythonprojects/crash_check.py
+++ pythonprojects/crash_check.py
@@ -5,14 +5,24 @@ import json
 import yfinance as yf
 
 crashdate = datetime.today()
-
-
+crash_check = ''
+# get the NASDAQ realtime index and calculate the movement percentage
 def statuscheck(ticker):
     msft = yf.Ticker(ticker)
     yesterday = msft.info['regularMarketPreviousClose']
+    print(yesterday)
     today = msft.info['regularMarketPrice']
+    print(today)
     return (today - yesterday)/yesterday
-print(statuscheck('^IXIC'))
+
+# print(statuscheck('^IXIC'))
+
+nasdaq_rate = statuscheck('^IXIC')
+if nasdaq_rate <= -0.03:
+    crash_check = 'y'
+else:
+    crash_check = 'n'
+print(nasdaq_rate)
 # update the config.ini file everyday
 config = configparser.ConfigParser()
 
@@ -28,9 +38,9 @@ config['PRICE'] = {
 }
 
 config['CRASH'] = {
-    'crash' : 'y',
+    'crash' : crash_check,
     'crashdate' : '2022-10-14',
-    'resetdate' : '2022-11-17'
+    'resetdate' : '2022-11-15'
 }
 
 with  open('config.ini', 'w') as configfile:
